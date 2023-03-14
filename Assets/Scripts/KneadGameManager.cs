9514abb8a88e187904bb94e34b84659b1d8ea0c4
gisaacs-trinity <gisaacs@trinity.edu>
2023-03-14 15:36:28 -0500

    Kneading Minigame Functionality

---
+++ Assets/Scripts/KneadGameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KneadGameManager : MonoBehaviour
+{
+    public GameObject dough;
+    public GameObject lumpManager;
+    public GameObject lumpPrefab;
+
+    public GameObject meter;
+    public GameObject winMenu;
+    public TMP_Text numLoafText;
+    public TMP_Text newCurrencyText;
+    public TMP_Text totalCurrencyText;
+
+    private int numLumps;
+    private int loavesKneaded = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numLumps = 40;
+        meter.GetComponent<Slider>().maxValue = numLumps;
+        lumpManager.GetComponent<LumpManager>().numLumps = numLumps;
+        winMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meter.GetComponent<Slider>().value = numLumps - lumpManager.GetComponent<LumpManager>().getLumpsRemaining();
+    }
+
+    public void WinGame()
+    {
+        Debug.Log("You Win");
+        loavesKneaded++;
+        winMenu.SetActive(true);
+        if (loavesKneaded == 1)
+        {
+            numLoafText.text = "1 Loaf Kneaded!";
+        }
+        else numLoafText.text = loavesKneaded + " Loaves Kneaded!";
+        
+    }
+
+    public void Restart()
+    {
+        winMenu.SetActive(false);
+        lumpManager.GetComponent<LumpManager>().NewLoaf();
+    }
+
+    public int getLoavesKneaded()
+    {
+        return loavesKneaded;
+    }
+}
