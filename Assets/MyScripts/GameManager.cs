abd54c924d60f4237fa4502e1198be7423e7a746
Justin Velasco <jvelasco@trinity.edu>
2023-04-03 15:31:29 -0500

    Intro dialogue only plays once; added yarn function to Game Manager.

--- Assets/MyScripts/GameManager.cs
+++ Assets/MyScripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Yarn.Unity;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class GameManager : MonoBehaviour
     //global variables
     //bread info
     //coin info
+    //yarn variables
+    static bool introDone = false;
+    static bool ingTutorial = false;
+    static bool kneadTutorial = false;
+    static bool ovenTutorial = false;
+    static bool dispTutorial = false;
 
     public UnityEvent onMiniGameCube = new UnityEvent();
 
@@ -29,5 +36,72 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
-    }   
+    }
+
+    // ------------ Yarn functions ------------
+
+    // Introduction Complete
+    [YarnFunction("getIntroDone")]
+    public static bool GetIntroDone()
+    {
+        return introDone;
+    }
+
+    [YarnCommand("setIntroDone")]
+    public static void SetIntroDone(bool val)
+    {
+        introDone = val;
+    }
+
+    // Ingredient Tutorial
+    [YarnFunction("getIngTutorial")]
+    public static bool GetIngTutorial()
+    {
+        return ingTutorial;
+    }
+
+    [YarnCommand("setIngTutorial")]
+    public static void SetIngTutorial(bool val)
+    {
+        ingTutorial = val;
+    }
+
+    // Kneading Tutorial
+    [YarnFunction("getKneadTutorial")]
+    public static bool GetKneadTutorial()
+    {
+        return kneadTutorial;
+    }
+
+    [YarnCommand("setKneadTutorial")]
+    public static void SetKneadTutorial(bool val)
+    {
+        kneadTutorial = val;
+    }
+
+    // Oven Tutorial
+    [YarnFunction("getOvenTutorial")]
+    public static bool GetOvenTutorial()
+    {
+        return ovenTutorial;
+    }
+
+    [YarnCommand("setOvenTutorial")]
+    public static void SetOvenTutorial(bool val)
+    {
+        ovenTutorial = val;
+    }
+
+    // Display Tutorial
+    [YarnFunction("getDispTutorial")]
+    public static bool GetDispTutorial()
+    {
+        return dispTutorial;
+    }
+
+    [YarnCommand("setDispTutorial")]
+    public static void SetDispTutorial(bool val)
+    {
+        dispTutorial = val;
+    }
 }
