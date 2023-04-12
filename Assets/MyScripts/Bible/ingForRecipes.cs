63849bfc12bf0b817ed3112d278a86c89f06cddc
Amanda Choi <achoi@trinity.edu>
2023-04-11 19:24:24 -0500

    inventory system bones

---
+++ Assets/MyScripts/Bible/ingForRecipes.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class ingForRecipes : MonoBehaviour
+{
+    int egg = 0;
+    int flour = 0;
+    int milk = 0;
+
+    //store ingrediant over sessions   
+    public void Setup()
+    {
+        //EGGS
+        if (egg <= PlayerPrefs.GetInt("egg"))
+        {
+            egg = PlayerPrefs.GetInt("egg");
+            Debug.Log("EGG: " + egg);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("egg", egg);
+            Debug.Log("EGG: " + egg);
+        }
+
+        //FLOUR
+        if (flour <= PlayerPrefs.GetInt("flour"))
+        {
+            flour = PlayerPrefs.GetInt("flour");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("flour", flour);
+        }
+
+        //MILK
+        if (milk <= PlayerPrefs.GetInt("milk"))
+        {
+            milk = PlayerPrefs.GetInt("milk");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("milk", milk);
+        }
+    }
+}
