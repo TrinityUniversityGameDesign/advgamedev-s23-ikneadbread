356dafd349d1bc49d53df7ad4fe2f6e806224bc9
Youngone Lee <youngone.lee@accenture.com>
2023-04-17 08:44:12 -0500

    turns on boots with mesh renderer

--- Assets/MyScripts/UiBullshit/UpgradeStore/EquiptItem.cs
+++ Assets/MyScripts/UiBullshit/UpgradeStore/EquiptItem.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class EquiptItem : MonoBehaviour
 {
-    private GameObject cowboyHat;
+    //private GameObject cowboyHat;
+    private GameObject BootBL, BootBR, BootFL, BootFR;
 
-    public Vector3 PickPosition;
-    public Vector3 PickRotation;
+    public Vector3 BLPickPosition;
+    public Vector3 BLPickRotation;
 
+    public Vector3 BRPickPosition;
+    public Vector3 BRPickRotation;
+
+    public Vector3 FLPickPosition;
+    public Vector3 FLPickRotation;
+
+    public Vector3 FRPickPosition;
+    public Vector3 FRPickRotation;
 
     private GameManager GM;
 
     private void Start()
     {
         GM = GameObject.FindObjectOfType<GameManager>();
-        cowboyHat = GameObject.Find("CowboyHat");
+        //cowboyHat = GameObject.Find("CowboyHat");
+
+        BootBL = GameObject.Find("BootBL");
+        BootBR = GameObject.Find("BootBR");
+        BootFL = GameObject.Find("BootFL");
+        BootFR = GameObject.Find("BootFR");
+
+        BootBL.GetComponent<Renderer>().enabled = false;
+        BootBR.GetComponent<Renderer>().enabled = false;
+        BootFL.GetComponent<Renderer>().enabled = false;
+        BootFR.GetComponent<Renderer>().enabled = false;
+
+
     }
 
     // Update is called once per frame
@@ -30,9 +51,20 @@ public class EquiptItem : MonoBehaviour
     public void EquiptBoots()
     {
         //apply vector3
-        cowboyHat.transform.parent = GameObject.Find("Head").transform;
-        cowboyHat.transform.localPosition = PickPosition;
-        cowboyHat.transform.localEulerAngles = PickRotation;
+        //cowboyHat.transform.parent = GameObject.Find("Head").transform;
+        //cowboyHat.transform.localPosition = PickPosition;
+        //cowboyHat.transform.localEulerAngles = PickRotation;
+
+        BootBL.GetComponent<Renderer>().enabled = true;
+        BootBR.GetComponent<Renderer>().enabled = true;
+        BootFL.GetComponent<Renderer>().enabled = true;
+        BootFR.GetComponent<Renderer>().enabled = true;
+
+
+        //BootBL.transform.parent = GameObject.Find("LegBL").transform;
+        //BootBL.transform.localPosition = BLPickPosition;
+        //BootBL.transform.localEulerAngles = BLPickRotation;
+
 
     }
 }
