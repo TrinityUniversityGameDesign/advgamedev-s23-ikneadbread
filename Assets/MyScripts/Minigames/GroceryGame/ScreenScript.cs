d9ebcb81102e578172adedecdfc3f93ccdf87c8a
Jazmine Kibet <jazifay@trinity.edu>
2023-04-18 09:13:21 -0500

    fixed play button

--- Assets/MyScripts/Minigames/GroceryGame/ScreenScript.cs
+++ Assets/MyScripts/Minigames/GroceryGame/ScreenScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class ScreenScript : MonoBehaviour
 {
 
@@ -62,5 +63,7 @@ public class ScreenScript : MonoBehaviour
     {
         EndScreen.SetActive(true);
     }
+
+
 
 }
