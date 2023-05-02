9eaeab531ffa2a1112a29abab7eb12e1e2e7a734
gisaacs-trinity <gisaacs@trinity.edu>
2023-05-02 18:12:53 -0500

    Can open inventory after new intro scene

--- Assets/MyScripts/UiBullshit/FromInherit.cs
+++ Assets/MyScripts/UiBullshit/FromInherit.cs
@@ -12,8 +12,9 @@ public class FromInherit : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider collide){
-        //GM = GameObject.Find("globalGM").GetComponent<GameManager>();
-        //GM.lastCoords = GM.planePos;//set last coords to where you want to spawn in the next scene
+        GM = GameObject.Find("globalGM").GetComponent<GameManager>();
+        GM.lastCoords = new Vector3(474.11f, 0.8f, 370);
+        GM.currScene = GameManager.travelDestination.NewHomeTown;
         SceneManager.LoadScene("NewHomeTown");
     }
 }
