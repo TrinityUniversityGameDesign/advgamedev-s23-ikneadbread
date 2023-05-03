d68d9d0cfaf68bf081f27bef173625bf75ec98cc
gisaacs-trinity <gisaacs@trinity.edu>
2023-05-03 17:01:55 -0500

    last debug button removed, winning shoudln't put you in the void

--- Assets/MyScripts/WinScreenScript.cs
+++ Assets/MyScripts/WinScreenScript.cs
@@ -10,7 +10,35 @@ public class WinScreenScript : MonoBehaviour
 
     public void continueGame()
     {
-        SceneManager.LoadScene(GM.townToReturn());
+        GM = GameObject.FindObjectOfType<GameManager>();
+        switch (GM.lastScene)
+        {
+            case (GameManager.travelDestination.Egypt):
+                GM.lastScene = GM.currScene;
+                GM.currScene = GameManager.travelDestination.Egypt;
+                GM.lastCoords = new Vector3(-532.916992f, 16.6599998f, 632.41803f);
+                SceneManager.LoadScene("Egypt");
+                break;
+            case (GameManager.travelDestination.Forest):
+                GM.lastScene = GM.currScene;
+                GM.currScene = GameManager.travelDestination.Forest;
+                GM.lastCoords = new Vector3(428.04776f, -0.0199999511f, 381.833923f);
+                SceneManager.LoadScene("Forest");
+                break;
+            case (GameManager.travelDestination.NewHomeTown):
+                GM.lastScene = GM.currScene;
+                GM.currScene = GameManager.travelDestination.NewHomeTown;
+                GM.lastCoords = new Vector3(459.420013f, 0.0289999992f, 451.269989f);
+                SceneManager.LoadScene("NewHomeTown");
+                break;
+            default:
+                // Same as HomeTown
+                GM.lastScene = GM.currScene;
+                GM.currScene = GameManager.travelDestination.NewHomeTown;
+                GM.lastCoords = new Vector3(459.420013f, 0.0289999992f, 451.269989f);
+                SceneManager.LoadScene("NewHomeTown");
+                break;
+        }
     }
 
     public void mainMenu()
